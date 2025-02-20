46cf5d0ad68dafd2b77a4d242f48202b194a8197
Sean Santiago <seant@MacBook-Air-89.local>
2025-02-20 15:50:52 -0500

    Remove dupes

--- in-class8-javas.js
+++
@@ -1,9 +0,0 @@
-console.log(5 + 'hello 5');
-
-console.log("the number 5 is turned into a string so that it can be concatenated");
-
-console.log(Number(true));
-console.log(Number(false));
-
-console.log("The 1 output means true, and the 0 output means false, and this is after these values are converted to numbers");
-
