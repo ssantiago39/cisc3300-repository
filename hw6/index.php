e8925d471099ff7473553e0d45e3682b6a404224
Sean Santiago <seant@MacBook-Air-206.fios-router.home>
2025-03-04 23:13:22 -0500

    hw6

---
+++ hw6/index.php
@@ -0,0 +1,36 @@
+<?php
+//index php file
+// 6a
+$me = [
+   "name" => "Sean Santiago",
+   "age" => 20,
+   "major" => "Computer Science",
+   "sign" => "virgo",
+   "university" => "Fordham University"
+];
+
+
+foreach ($me as $key => $value) {
+    echo "$key: $value <br>";
+}
+
+
+
+
+// 7
+function multiplyNumber(int $num, int $multiplier = 2): int {
+   return $num * $multiplier;
+}
+
+
+echo "15 multiplied by 11: " . multiplyNumber(15, 11) . "<br>";
+echo "17 multiplied by (2): " . multiplyNumber(17) . "<br>";  
+
+
+// 8
+
+
+readfile("resources/page.html");
+
+
+?>
