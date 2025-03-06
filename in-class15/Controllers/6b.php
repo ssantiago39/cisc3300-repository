9f560e8d74795b17d024e2692db35463da499968
Sean Santiago <seant@MacBook-Air-262.local>
2025-03-06 17:00:30 -0500

    _

--- in-class15/Controllers/6b.php
+++ in-class15/Controllers/6b.php
@@ -1,10 +1,10 @@
 <?php
 require_once '6a.php';
 
-//movie instead of post
-class MovieController {
-    public function listMovies() {
-        return (new Movie())->getMovies();
+
+class PostController {
+    public function listPosts() {
+        return (new Post())->getPosts();
     }
 }
 
