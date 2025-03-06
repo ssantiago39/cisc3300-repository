e6f723e5b513aec4b8614dccbb6b4e21c8a64189
Sean Santiago <seant@MacBook-Air-262.local>
2025-03-06 16:51:25 -0500

    _

---
+++ in-class15/Models/6a.php
@@ -0,0 +1,13 @@
+<?php
+
+//used movies instead of posts
+class Movie {
+    public function getMovies() {
+        return [
+            ["title" => "Harry Potter", "description" => "Kid with big glasses."],
+            ["title" => "The Matrix", "description" => " Is this the future??."]
+        ];
+    }
+}
+
+//_
