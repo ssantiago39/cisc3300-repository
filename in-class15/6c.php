274c996176fdc7f79628c678e004a439cbc74686
Sean Santiago <seant@MacBook-Air-262.local>
2025-03-06 17:01:58 -0500

    _

--- in-class15/6c.php
+++ in-class15/6c.php
@@ -1,9 +1,8 @@
 <?php
 require_once '6b.php';
 
-//did movies instead of posts
-if ($_SERVER['REQUEST_URI'] == '/movies') {
-    echo json_encode((new MovieController())->listMovies());
+if ($_SERVER['REQUEST_URI'] == '/posts') {
+    echo json_encode((new PostController())->listPosts());
 } else {
     http_response_code(404);
     echo json_encode(["error" => "Not Found"]);
