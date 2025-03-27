690994a6bef76c698c369cbcc391c302f02bc0db
Sean Santiago <seant@MacBook-Air-262.local>
2025-03-27 17:06:48 -0400

    _

---
+++ in-class19/post.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `posts` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(20) NOT NULL,
+    `description` TEXT NOT NULL,
+    primary key (`id`)
+);
+
+insert into `posts` (`name`, `description`) 
+values('Hello', 'fjenjnfjns'),
+ 
