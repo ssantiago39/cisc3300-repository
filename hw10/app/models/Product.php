f57eb894cee11fdf3ac0a0a12e91cda0ba8c5217
Sean Santiago <seant@MacBook-Air-334.fios-router.home>
2025-04-10 20:25:27 -0400

    10

---
+++ hw10/app/models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+// using the database class namespace
+use app\models\Model;
+
+class Product extends Model {
+
+    public function getAllProductsByFlavor($flavor) {
+        $query = "select * from IceCream where flavor like :flavor";
+        return $this->query($query, [
+            'flavor' => '%' . $flavor . '%',
+        ]);
+    }
+
+    public function getAllProducts() {
+        $query = "select * from IceCream";
+        return $this->query($query);
+    }
+
+    public function getProductById($id) {
+        $query = "select * from IceCream where id = :id";
+        return $this->query($query, ['id' => $id]);
+    }
+
+    public function saveProduct($inputData) {
+        $query = "insert into IceCream (flavor, price) values (:flavor, :price);";
+        return $this->query($query, $inputData);
+    }
+
+    public function updateProduct($inputData) {
+        $query = "update IceCream set flavor = :flavor, price = :price where id = :id";
+        return $this->query($query, $inputData);
+    }
+
+    public function deleteProduct($inputData) {
+        $query = "DELETE FROM IceCream where id = :id";
+        return $this->query($query, $inputData);
+    }
+}
